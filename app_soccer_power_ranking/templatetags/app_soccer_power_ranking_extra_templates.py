272c361972131c9ed02a94846ef11d08c1762d22
Exergos <info@exergos.be>
2014-12-20 00:28:09 +0100

    Added extended qtip library

--- app_soccer_power_ranking/templatetags/app_soccer_power_ranking_extra_templates.py
+++ app_soccer_power_ranking/templatetags/app_soccer_power_ranking_extra_templates.py
@@ -6,4 +6,4 @@ register = template.Library()
 # This filter makes sure you can access lists in template tags
 @register.filter
 def get_at_index(list, index):
-    return list[index]
+    return list[index-1] # "index-1" because forloop.counter in django template is 1-indexed
